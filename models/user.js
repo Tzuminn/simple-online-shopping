f6e24e89ed26f0cdfa9ec803c29d9ce9cc37ac1a
linway035 <wayneck0138@gmail.com>
2023-01-11 21:05:33 +0800

    feat: 建立User table

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    birthday: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN // 新增這句
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users', // 新增這句
+    underscored: true
+  })
+  return User
+}
