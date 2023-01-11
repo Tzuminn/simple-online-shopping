e046230835996edb41e155af7566493d8cf14034
linway035 <wayneck0138@gmail.com>
2023-01-11 23:37:08 +0800

    feat: 建立Category table

---
+++ migrations/20230111153555-create-category.js
@@ -0,0 +1,29 @@
+// npx sequelize model:generate --name Category --attributes name:string --underscored
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories')
+  }
+}
