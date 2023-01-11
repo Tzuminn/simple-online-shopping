931b9d69a384205cefb0eb64a2c90e37549f8a5c
linway035 <wayneck0138@gmail.com>
2023-01-11 23:35:00 +0800

    feat: 建立Product table

---
+++ migrations/20230111153232-create-product.js
@@ -0,0 +1,35 @@
+// npx sequelize model:generate --name Product --attributes name:string,price:integer,description:string --underscored
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products')
+  }
+}
