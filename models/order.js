64a092011c7294e2619a3b13a270291c1c1cd205
linway035 <wayneck0138@gmail.com>
2023-01-12 13:22:42 +0800

    feat: 建立 order_Id 關聯

--- models/order.js
+++ models/order.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Order.hasMany(models.OrderDetail, { foreignKey: 'orderId' })
     }
   }
   Order.init({
--- models/orderdetail.js
+++ models/orderdetail.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      OrderDetail.belongsTo(models.Order, { foreignKey: 'orderId' })
     }
   }
   OrderDetail.init({
