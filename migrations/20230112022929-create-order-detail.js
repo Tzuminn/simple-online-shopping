4e14a79973a3e7ec9b63f53ac29f5aa4ba382398
linway035 <wayneck0138@gmail.com>
2023-01-12 10:48:33 +0800

    feat: 建立OrderDetail table

---
+++ migrations/20230112022929-create-order-detail.js
@@ -0,0 +1,29 @@
+// npx sequelize model:generate --name OrderDetail --attributes orderQuantity:integer --underscored
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('OrderDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_quantity: { // 修改為snake_case
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('OrderDetails')
+  }
+}
---
+++ models/orderdetail.js
@@ -0,0 +1,25 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class OrderDetail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  OrderDetail.init({
+    orderQuantity: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'OrderDetail',
+    tableName: 'OrderDetails',
+    underscored: true
+  })
+  return OrderDetail
+}
