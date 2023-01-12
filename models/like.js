fbadf891420539ae23334499bcc34b6cad2ddf0f
linway035 <wayneck0138@gmail.com>
2023-01-12 11:02:22 +0800

    feat: 建立Like table

---
+++ models/like.js
@@ -0,0 +1,26 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Like extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Like.init({
+    userId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Like',
+    tableName: 'Likes',
+    underscored: true
+  })
+  return Like
+}
