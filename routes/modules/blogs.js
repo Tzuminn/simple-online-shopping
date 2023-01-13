3be5d288ef5f62120274f1c4497e1b3f9113bb18
Min <113697199+Tzuminn@users.noreply.github.com>
2023-01-13 11:00:52 +0800

    feat: 新增routes及controllers架構 (#3)

--- routes/modules/blogs.js
+++ routes/modules/blogs.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const blogController = require('../../controllers/blog-controller')
+
+router.get('/', blogController.getBlogs)
+
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const userController = require('../../controllers/user-controller')
+const authenticated = require('../../middleware/auth')
+
+
+router.post('/login', userController.login)
+router.post('/orders', authenticated, userController.postOrders)
+
 module.exports = router
