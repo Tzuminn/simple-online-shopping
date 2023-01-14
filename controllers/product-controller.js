09c68a8362ed492d0406babb41841af06ab2f0ee
linway035 <wayneck0138@gmail.com>
2023-01-14 13:34:56 +0800

    feat: 試寫最新商品 & run lint

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,9 +1,23 @@
+const { Product } = require('../models')
 const productController = {
   getBestSell: async (req, res, next) => {
-
   },
   getNewest: async (req, res, next) => {
-
+    try {
+      const page = Number(req.query.page) || 1
+      const limit = 10
+      const offset = (page - 1) * limit
+      const products = await Product.findAll({
+        order: [['createdAt', 'DESC'], ['id', 'ASC']],
+        limit,
+        offset,
+        raw: true,
+        nest: true
+      })
+      res.status(200).json(products)
+    } catch (err) {
+      next(err)
+    }
   },
   getSortByPrice: async (req, res, next) => {
 
