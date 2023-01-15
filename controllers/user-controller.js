0f698bc7acb89117f9a8df62e3e3c506c2372008
JoyWanddrr <m81y102@msn.com>
2023-01-15 19:23:06 +0800

    fix:Facebook、GOOGLE路由關閉session

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,18 @@
+// const { User, Order, Orderdetail, Product } = require('../models')
+
 const userController = {
-  login: async (req, res, next) => {
+  // login: async (req, res, next) => {
 
-  },
+  // },
   postOrders: async (req, res, next) => {
-
+    // 前端傳回的資料
+    // const { name } = req.body
+    // 錯誤驗證是否要另外寫
+    // 將資料放入兩個不同資料庫?Cart回傳
+    // 產生訂單編號
+    // OrderDetail.create
+    // 攜帶token回傳
+    // return res.status(200).json({ status: 'success', data: token })
   }
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const userController = require('../../controllers/user-controller')
+// const userController = require('../../controllers/user-controller')
 // const authenticated = require('../../middleware/auth')
 
 // router.post('/orders', authenticated, userController.postOrders)
-router.post('/orders', userController.postOrders)
+// router.post('/orders', userController.postOrders)
 
 module.exports = router
