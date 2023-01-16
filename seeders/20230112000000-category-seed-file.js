6fc6326bb085f34dda79be5d7b078a0c3b7fccf3
Min <113697199+Tzuminn@users.noreply.github.com>
2023-01-16 15:45:23 +0800

    B03 seeders (#7)
    * feat: 新增payment種子資料
    
    * feat: 新增delivery種子資料
    
    * fix: 補上所有種子資料第二參數null
    
    * Update 20230113082225-product-seed-file.js
    
    Category_id改成大寫

--- seeders/20230112000000-category-seed-file.js
+++ seeders/20230112000000-category-seed-file.js
@@ -12,6 +12,6 @@ module.exports = {
       })), {})
   },
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Categories', {})
+    await queryInterface.bulkDelete('Categories', null, {})
   }
 }
