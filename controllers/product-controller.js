3069543a4776f0215fcae69bbd5664d56552b099
linway035 <wayneck0138@gmail.com>
2023-01-16 16:10:11 +0800

    feat: 修改銷售數量為sales，圖片加上isCover

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,18 +1,19 @@
 const { sequelize, Product, Category, Image } = require('../models')
+const displayLimit = 10
+
 const productController = {
   getBestSell: async (req, res, next) => {
     try {
       const page = Number(req.query.page) || 1
-      const limit = 10
-      const offset = (page - 1) * limit
+      const offset = (page - 1) * displayLimit
       const products = await Product.findAll({
         include: [
           { model: Category, attributes: ['id', 'name'] },
           { model: Image, attributes: ['id', 'ProductId', 'url', 'isCover'], where: { isCover: true } }
         ],
-        attributes: ['id', 'name', 'price', 'createdAt', [sequelize.literal('(SELECT IFNULL(SUM(order_quantity),0) FROM OrderDetails WHERE Product_id = Product.id )'), 'orderQuantity']],
-        order: [[sequelize.literal('orderQuantity'), 'DESC'], ['id', 'DESC']],
-        limit,
+        attributes: ['id', 'name', 'price', [sequelize.literal('(SELECT IFNULL(SUM(order_quantity),0) FROM OrderDetails WHERE Product_id = Product.id )'), 'sales']],
+        order: [[sequelize.literal('sales'), 'DESC'], ['id', 'DESC']],
+        displayLimit,
         offset,
         raw: true,
         nest: true
@@ -25,16 +26,15 @@ const productController = {
   getNewest: async (req, res, next) => {
     try {
       const page = Number(req.query.page) || 1
-      const limit = 10
-      const offset = (page - 1) * limit
+      const offset = (page - 1) * displayLimit
       const products = await Product.findAll({
         include: [
           { model: Category, attributes: ['id', 'name'] },
           { model: Image, attributes: ['id', 'ProductId', 'url', 'isCover'], where: { isCover: true } }
         ],
-        attributes: ['id', 'name', 'price', 'createdAt', [sequelize.literal('(SELECT IFNULL(SUM(order_quantity),0) FROM OrderDetails WHERE Product_id = Product.id )'), 'orderQuantity']],
+        attributes: ['id', 'name', 'price', 'createdAt'],
         order: [['createdAt', 'DESC'], ['id', 'DESC']],
-        limit,
+        displayLimit,
         offset,
         raw: true,
         nest: true
@@ -47,16 +47,15 @@ const productController = {
   getSortByPrice: async (req, res, next) => {
     try {
       const page = Number(req.query.page) || 1
-      const limit = 10
-      const offset = (page - 1) * limit
+      const offset = (page - 1) * displayLimit
       const products = await Product.findAll({
         include: [
           { model: Category, attributes: ['id', 'name'] },
           { model: Image, attributes: ['id', 'ProductId', 'url', 'isCover'], where: { isCover: true } }
         ],
-        attributes: ['id', 'name', 'price', 'createdAt', [sequelize.literal('(SELECT IFNULL(SUM(order_quantity),0) FROM OrderDetails WHERE Product_id = Product.id )'), 'orderQuantity']],
+        attributes: ['id', 'name', 'price'],
         order: [['price', 'DESC'], ['id', 'DESC']],
-        limit,
+        displayLimit,
         offset,
         raw: true,
         nest: true
@@ -77,7 +76,7 @@ const productController = {
         raw: true,
         nest: true
       })
-      const images = await Image.findAll({ where: { ProductId: productId }, attributes: ['id', 'url'], raw: true, nest: true })
+      const images = await Image.findAll({ where: { ProductId: productId }, attributes: ['id', 'url', 'isCover'], raw: true, nest: true })
       product.Image = images
       if (!product) throw new Error('此商品不存在')
       res.status(200).json(product)
