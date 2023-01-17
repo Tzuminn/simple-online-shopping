04bd48b15fce00d9755b7fb5dc13d3b4848a8346
linway035 <wayneck0138@gmail.com>
2023-01-17 15:56:53 +0800

    feat: 新增基礎的auth

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const session = require('express-session')
 const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
-
-const session = require('express-session')
 
 const corsOptions = {
   origin: [
