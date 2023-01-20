86157f04540db1e889963ba9d3dc3ae5e361e1e7
Tzuminn <valuesu0326@gmail.com>
2023-01-20 10:16:02 +0800

    feat: 新增產品上架功能

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const { Product, Image } = require('../models')
+const imgurFileHandler = require('../helpers/file-helpers')
 
 const adminController = {
   login: async (req, res, next) => {
@@ -23,11 +25,58 @@ const adminController = {
   deleteProduct: async (req, res, next) => {
 
   },
-  getProducts: async (req, res, next) => {
-
-  },
   postProduct: async (req, res, next) => {
+    try {
+      const { name, price, description, CategoryId } = req.body
+      if (!name.trim() || !price.trim() || !description.trim() || !CategoryId.trim()) throw new Error('所有資料都是必填')
+      const newProduct = await Product.create({
+        name,
+        price,
+        description,
+        CategoryId
+      })
+      const productData = newProduct.toJSON()
 
+      const { files } = req
+      const ProductImgUpload = []
+      for (let i = 0; i < 6; i++) {
+        ProductImgUpload.push(await imgurFileHandler(files[i]))
+      }
+      // console.log('ProductImgUpload', ProductImgUpload)
+      let productImgData = []
+      // await ProductImgUpload.map(file => {
+      //   const newProductImg = Image.bulkCreate([{
+      //     url: ProductImgUpload[file],
+      //     ProductId: productData.id
+      //   }])
+      //   productImgData = newProductImg
+      //   return productImgData
+      // })
+      productImgData = await Image.bulkCreate([{
+        url: ProductImgUpload[0],
+        ProductId: productData.id,
+        isCover: 1
+      }, {
+        url: ProductImgUpload[1],
+        ProductId: productData.id
+      }, {
+        url: ProductImgUpload[2],
+        ProductId: productData.id
+      }, {
+        url: ProductImgUpload[3],
+        ProductId: productData.id
+      }, {
+        url: ProductImgUpload[4],
+        ProductId: productData.id
+      }, {
+        url: ProductImgUpload[5],
+        ProductId: productData.id
+      }])
+
+      res.status(200).json({ product: productData, image: productImgData })
+    } catch (err) {
+      next(err)
+    }
   },
   getOrders: async (req, res, next) => {
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../../controllers/user-controller')
-const { authenticated } = require('../../middleware/auth')
+const { authenticated, authenticatedUser } = require('../../middleware/auth')
 
 router.post('/login', userController.login)
-router.post('/orders', authenticated, userController.postOrders)
+router.post('/orders', authenticated, authenticatedUser, userController.postOrders)
 
 module.exports = router
