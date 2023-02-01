66bbc4c6f56daed91cb08f16c23cee111801f9e8
linway035 <wayneck0138@gmail.com>
2023-02-02 00:34:02 +0800

    feat: 補回auth

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const { OrderValidator } = require('../../middleware/validator')
 
 const userController = require('../../controllers/user-controller')
-// const { authenticated } = require('../../middleware/auth')
+const { authenticated } = require('../../middleware/auth')
 
-router.post('/orders',  OrderValidator, userController.postOrders)
+router.post('/orders', authenticated, OrderValidator, userController.postOrders)
 // 訂單查詢
-router.get('/orders',  userController.getOrders)
+router.get('/orders', authenticated, userController.getOrders)
 
 module.exports = router
