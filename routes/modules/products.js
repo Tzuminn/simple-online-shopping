6d4e9c085704dcabb9ebbd84d812faad32437c59
linway035 <wayneck0138@gmail.com>
2023-02-04 14:42:02 +0800

    feat: 移除auth

--- routes/modules/products.js
+++ routes/modules/products.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 const productController = require('../../controllers/product-controller')
 
-router.get('/all/bestsell', authenticatedAdmin,productController.getBestSell)
+router.get('/all/bestsell', productController.getBestSell)
 router.get('/all/newest', productController.getNewest)
 router.get('/all/sortbyprice', productController.getSortByPrice)
 
