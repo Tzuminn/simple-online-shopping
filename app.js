321a12675cd148efa6300add458bd0f5f36e4183
Tzuminn <valuesu0326@gmail.com>
2023-02-15 15:21:00 +0800

    feat: 新增機器人回覆自定義內容

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(express.json({ limit: '5000mb' }))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 app.use('/api', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/modules/messaging.js
+++ routes/modules/messaging.js
@@ -11,21 +11,31 @@ router.get('/messaging', (req, res) => {
     req.query['hub.verify_token'] === process.env.MESSENGER_VERIFY_TOKEN
   ) {
     res.send(req.query['hub.challenge'])
+    console.log('req.query:', req.query)
   } else {
     console.error('Failed validation. Make sure the validation tokens match.')
     res.sendStatus(403)
   }
 })
 
-router.post('/messaging', (req, res) => {
+router.post('/messaging', async (req, res) => {
   const event = req.body.entry[0].messaging[0]
   const userId = event.sender.id
   const { text } = event.message
-  console.log('event:', event)
-  console.log('userId:', userId)
-  console.log('text:', text)
-  client.sendText(userId, text)
-  res.sendStatus(200)
+
+  if (text.includes('官網')) {
+    client.sendText(userId, '官網連結:https://beark0515.github.io/pets-store。')
+    res.status(200)
+  } else if (text.includes('訂單查詢')) {
+    client.sendText(userId, '訂單查詢連結:https://beark0515.github.io/pets-store，請輸入訂單編號。')
+    res.status(200)
+  } else if (text.includes('其它問題')) {
+    client.sendText(userId, '此為自動回覆訊息，請留下您的問題並靜候客服人員回覆，謝謝。')
+    res.status(200)
+  } else {
+    client.sendText(userId, '您可以輸入"官網"、"訂單查詢"、"其它問題"......等關鍵字。')
+    res.status(200)
+  }
 })
 
 module.exports = router
